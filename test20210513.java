7694764fed278358bb1d707ffe60970e2548b574
chadpirate7 <83903900+chadpirate7@users.noreply.github.com>
2021-05-13 10:48:30 +0900

    Add files via upload

---
+++ test20210513.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class test20210513 {
+	public static void main(String[] args) {
+		
+		for (int y =0; y < 5; y++) {
+			for (int x = 0; x<10; x++)
+				System.out.println("");
+		}
+	}
+
+}
